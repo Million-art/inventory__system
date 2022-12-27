24c559b22173f42aba4238d6f87d6ee26a74e49e
Million-art <74927604+Million-art@users.noreply.github.com>
2022-12-27 22:44:40 +0300

    login page crated

---
+++ Inventory/Form1.Designer.vb
@@ -0,0 +1,385 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TextBoxBalance = New System.Windows.Forms.TextBox()
+        Me.TextBoxAccountStatys = New System.Windows.Forms.TextBox()
+        Me.TextBoxVatNumber = New System.Windows.Forms.TextBox()
+        Me.TextBoxEmail = New System.Windows.Forms.TextBox()
+        Me.TextBoxName = New System.Windows.Forms.TextBox()
+        Me.TextBoxMobile = New System.Windows.Forms.TextBox()
+        Me.TextBoxAddress = New System.Windows.Forms.TextBox()
+        Me.TextBoxAc = New System.Windows.Forms.TextBox()
+        Me.LabelccountStatus = New System.Windows.Forms.Label()
+        Me.LabelBalance = New System.Windows.Forms.Label()
+        Me.LabelVatNumber = New System.Windows.Forms.Label()
+        Me.LabelName = New System.Windows.Forms.Label()
+        Me.LabelAddress = New System.Windows.Forms.Label()
+        Me.LabelMobile = New System.Windows.Forms.Label()
+        Me.LabelEmail = New System.Windows.Forms.Label()
+        Me.LabelAc = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.BtnReset = New System.Windows.Forms.Button()
+        Me.BtnExit = New System.Windows.Forms.Button()
+        Me.BtnLogin = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.LabelPassword = New System.Windows.Forms.Label()
+        Me.LabelUsername = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.TabPage3 = New System.Windows.Forms.TabPage()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Controls.Add(Me.TabPage3)
+        Me.TabControl1.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.TabControl1.Location = New System.Drawing.Point(39, 33)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(749, 405)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.BackColor = System.Drawing.Color.White
+        Me.TabPage1.Controls.Add(Me.TextBoxBalance)
+        Me.TabPage1.Controls.Add(Me.GroupBox1)
+        Me.TabPage1.Controls.Add(Me.TextBoxAccountStatys)
+        Me.TabPage1.Controls.Add(Me.TextBoxVatNumber)
+        Me.TabPage1.Controls.Add(Me.TextBoxEmail)
+        Me.TabPage1.Controls.Add(Me.TextBoxName)
+        Me.TabPage1.Controls.Add(Me.TextBoxMobile)
+        Me.TabPage1.Controls.Add(Me.TextBoxAddress)
+        Me.TabPage1.Controls.Add(Me.TextBoxAc)
+        Me.TabPage1.Controls.Add(Me.LabelccountStatus)
+        Me.TabPage1.Controls.Add(Me.LabelBalance)
+        Me.TabPage1.Controls.Add(Me.LabelVatNumber)
+        Me.TabPage1.Controls.Add(Me.LabelName)
+        Me.TabPage1.Controls.Add(Me.LabelAddress)
+        Me.TabPage1.Controls.Add(Me.LabelMobile)
+        Me.TabPage1.Controls.Add(Me.LabelEmail)
+        Me.TabPage1.Controls.Add(Me.LabelAc)
+        Me.TabPage1.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 39)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(741, 362)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Customer Detail"
+        '
+        'TextBoxBalance
+        '
+        Me.TextBoxBalance.Location = New System.Drawing.Point(535, 117)
+        Me.TextBoxBalance.Name = "TextBoxBalance"
+        Me.TextBoxBalance.Size = New System.Drawing.Size(174, 35)
+        Me.TextBoxBalance.TabIndex = 16
+        '
+        'TextBoxAccountStatys
+        '
+        Me.TextBoxAccountStatys.Location = New System.Drawing.Point(535, 241)
+        Me.TextBoxAccountStatys.Name = "TextBoxAccountStatys"
+        Me.TextBoxAccountStatys.Size = New System.Drawing.Size(174, 35)
+        Me.TextBoxAccountStatys.TabIndex = 15
+        '
+        'TextBoxVatNumber
+        '
+        Me.TextBoxVatNumber.Location = New System.Drawing.Point(535, 178)
+        Me.TextBoxVatNumber.Name = "TextBoxVatNumber"
+        Me.TextBoxVatNumber.Size = New System.Drawing.Size(174, 35)
+        Me.TextBoxVatNumber.TabIndex = 14
+        '
+        'TextBoxEmail
+        '
+        Me.TextBoxEmail.Location = New System.Drawing.Point(535, 65)
+        Me.TextBoxEmail.Name = "TextBoxEmail"
+        Me.TextBoxEmail.Size = New System.Drawing.Size(174, 35)
+        Me.TextBoxEmail.TabIndex = 13
+        '
+        'TextBoxName
+        '
+        Me.TextBoxName.Location = New System.Drawing.Point(123, 117)
+        Me.TextBoxName.Name = "TextBoxName"
+        Me.TextBoxName.Size = New System.Drawing.Size(174, 35)
+        Me.TextBoxName.TabIndex = 12
+        '
+        'TextBoxMobile
+        '
+        Me.TextBoxMobile.Location = New System.Drawing.Point(123, 241)
+        Me.TextBoxMobile.Name = "TextBoxMobile"
+        Me.TextBoxMobile.Size = New System.Drawing.Size(174, 35)
+        Me.TextBoxMobile.TabIndex = 11
+        '
+        'TextBoxAddress
+        '
+        Me.TextBoxAddress.Location = New System.Drawing.Point(123, 178)
+        Me.TextBoxAddress.Name = "TextBoxAddress"
+        Me.TextBoxAddress.Size = New System.Drawing.Size(174, 35)
+        Me.TextBoxAddress.TabIndex = 10
+        '
+        'TextBoxAc
+        '
+        Me.TextBoxAc.Location = New System.Drawing.Point(123, 65)
+        Me.TextBoxAc.Name = "TextBoxAc"
+        Me.TextBoxAc.Size = New System.Drawing.Size(174, 35)
+        Me.TextBoxAc.TabIndex = 9
+        '
+        'LabelccountStatus
+        '
+        Me.LabelccountStatus.AutoSize = True
+        Me.LabelccountStatus.Location = New System.Drawing.Point(359, 241)
+        Me.LabelccountStatus.Name = "LabelccountStatus"
+        Me.LabelccountStatus.Size = New System.Drawing.Size(161, 30)
+        Me.LabelccountStatus.TabIndex = 8
+        Me.LabelccountStatus.Text = "Account Status"
+        '
+        'LabelBalance
+        '
+        Me.LabelBalance.AutoSize = True
+        Me.LabelBalance.Location = New System.Drawing.Point(359, 120)
+        Me.LabelBalance.Name = "LabelBalance"
+        Me.LabelBalance.Size = New System.Drawing.Size(88, 30)
+        Me.LabelBalance.TabIndex = 7
+        Me.LabelBalance.Text = "Balance"
+        '
+        'LabelVatNumber
+        '
+        Me.LabelVatNumber.AutoSize = True
+        Me.LabelVatNumber.Location = New System.Drawing.Point(359, 178)
+        Me.LabelVatNumber.Name = "LabelVatNumber"
+        Me.LabelVatNumber.Size = New System.Drawing.Size(131, 30)
+        Me.LabelVatNumber.TabIndex = 6
+        Me.LabelVatNumber.Text = "Vat Number"
+        '
+        'LabelName
+        '
+        Me.LabelName.AutoSize = True
+        Me.LabelName.Location = New System.Drawing.Point(22, 120)
+        Me.LabelName.Name = "LabelName"
+        Me.LabelName.Size = New System.Drawing.Size(71, 30)
+        Me.LabelName.TabIndex = 5
+        Me.LabelName.Text = "Name"
+        '
+        'LabelAddress
+        '
+        Me.LabelAddress.AutoSize = True
+        Me.LabelAddress.Location = New System.Drawing.Point(22, 178)
+        Me.LabelAddress.Name = "LabelAddress"
+        Me.LabelAddress.Size = New System.Drawing.Size(91, 30)
+        Me.LabelAddress.TabIndex = 4
+        Me.LabelAddress.Text = "Address"
+        '
+        'LabelMobile
+        '
+        Me.LabelMobile.AutoSize = True
+        Me.LabelMobile.Location = New System.Drawing.Point(22, 241)
+        Me.LabelMobile.Name = "LabelMobile"
+        Me.LabelMobile.Size = New System.Drawing.Size(82, 30)
+        Me.LabelMobile.TabIndex = 3
+        Me.LabelMobile.Text = "Mobile"
+        '
+        'LabelEmail
+        '
+        Me.LabelEmail.AutoSize = True
+        Me.LabelEmail.Location = New System.Drawing.Point(359, 65)
+        Me.LabelEmail.Name = "LabelEmail"
+        Me.LabelEmail.Size = New System.Drawing.Size(66, 30)
+        Me.LabelEmail.TabIndex = 2
+        Me.LabelEmail.Text = "Email"
+        '
+        'LabelAc
+        '
+        Me.LabelAc.AutoSize = True
+        Me.LabelAc.Location = New System.Drawing.Point(22, 65)
+        Me.LabelAc.Name = "LabelAc"
+        Me.LabelAc.Size = New System.Drawing.Size(50, 30)
+        Me.LabelAc.TabIndex = 1
+        Me.LabelAc.Text = "A/C"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.BtnReset)
+        Me.GroupBox1.Controls.Add(Me.BtnExit)
+        Me.GroupBox1.Controls.Add(Me.BtnLogin)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.LabelPassword)
+        Me.GroupBox1.Controls.Add(Me.LabelUsername)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(287, 311)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(749, 422)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        '
+        'BtnReset
+        '
+        Me.BtnReset.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.BtnReset.Location = New System.Drawing.Point(290, 325)
+        Me.BtnReset.Name = "BtnReset"
+        Me.BtnReset.Size = New System.Drawing.Size(110, 42)
+        Me.BtnReset.TabIndex = 7
+        Me.BtnReset.Text = "Reset"
+        Me.BtnReset.UseVisualStyleBackColor = True
+        '
+        'BtnExit
+        '
+        Me.BtnExit.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.BtnExit.Location = New System.Drawing.Point(434, 325)
+        Me.BtnExit.Name = "BtnExit"
+        Me.BtnExit.Size = New System.Drawing.Size(110, 42)
+        Me.BtnExit.TabIndex = 6
+        Me.BtnExit.Text = "Exit"
+        Me.BtnExit.UseVisualStyleBackColor = True
+        '
+        'BtnLogin
+        '
+        Me.BtnLogin.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.BtnLogin.Location = New System.Drawing.Point(142, 325)
+        Me.BtnLogin.Name = "BtnLogin"
+        Me.BtnLogin.Size = New System.Drawing.Size(110, 42)
+        Me.BtnLogin.TabIndex = 5
+        Me.BtnLogin.Text = "Login"
+        Me.BtnLogin.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(322, 259)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(222, 35)
+        Me.TextBox2.TabIndex = 4
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(322, 197)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(222, 35)
+        Me.TextBox1.TabIndex = 3
+        '
+        'LabelPassword
+        '
+        Me.LabelPassword.AutoSize = True
+        Me.LabelPassword.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.LabelPassword.Location = New System.Drawing.Point(142, 252)
+        Me.LabelPassword.Name = "LabelPassword"
+        Me.LabelPassword.Size = New System.Drawing.Size(105, 30)
+        Me.LabelPassword.TabIndex = 2
+        Me.LabelPassword.Text = "Password"
+        '
+        'LabelUsername
+        '
+        Me.LabelUsername.AutoSize = True
+        Me.LabelUsername.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.LabelUsername.Location = New System.Drawing.Point(142, 190)
+        Me.LabelUsername.Name = "LabelUsername"
+        Me.LabelUsername.Size = New System.Drawing.Size(110, 30)
+        Me.LabelUsername.TabIndex = 1
+        Me.LabelUsername.Text = "Username"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(203, 103)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(308, 47)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Inventory System"
+        '
+        'TabPage2
+        '
+        Me.TabPage2.BackColor = System.Drawing.Color.White
+        Me.TabPage2.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 39)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(741, 362)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Default Setting"
+        '
+        'TabPage3
+        '
+        Me.TabPage3.BackColor = System.Drawing.Color.White
+        Me.TabPage3.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.TabPage3.Location = New System.Drawing.Point(4, 39)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage3.Size = New System.Drawing.Size(741, 362)
+        Me.TabPage3.TabIndex = 2
+        Me.TabPage3.Text = "Cradit Control"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents TabPage3 As TabPage
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents BtnReset As Button
+    Friend WithEvents BtnExit As Button
+    Friend WithEvents BtnLogin As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents LabelPassword As Label
+    Friend WithEvents LabelUsername As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBoxBalance As TextBox
+    Friend WithEvents TextBoxAccountStatys As TextBox
+    Friend WithEvents TextBoxVatNumber As TextBox
+    Friend WithEvents TextBoxEmail As TextBox
+    Friend WithEvents TextBoxName As TextBox
+    Friend WithEvents TextBoxMobile As TextBox
+    Friend WithEvents TextBoxAddress As TextBox
+    Friend WithEvents TextBoxAc As TextBox
+    Friend WithEvents LabelccountStatus As Label
+    Friend WithEvents LabelBalance As Label
+    Friend WithEvents LabelVatNumber As Label
+    Friend WithEvents LabelName As Label
+    Friend WithEvents LabelAddress As Label
+    Friend WithEvents LabelMobile As Label
+    Friend WithEvents LabelEmail As Label
+    Friend WithEvents LabelAc As Label
+End Class
---
+++ Inventory/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Private Sub BtnReset_Click(sender As Object, e As EventArgs) Handles BtnReset.Click
+        TextBox1.Clear()
+        TextBox2.Clear()
+    End Sub
+
+    Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles BtnExit.Click
+        Dim result = MessageBox.Show("are you sure do you wont to exit? ", "closing exam", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If (result = DialogResult.Yes) Then
+            Application.Exit()
+        End If
+    End Sub
+
+    Private Sub BtnLogin_Click(sender As Object, e As EventArgs) Handles BtnLogin.Click
+        If TextBox1.Text = "million" And TextBox2.Text = "1234" Then
+            GroupBox1.Visible = False
+        Else
+            MessageBox.Show("incorect username or password")
+            TextBox1.Focus()
+        End If
+    End Sub
+End Class
